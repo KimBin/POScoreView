683a77cde445caca1887158510097b90c149e28b
jinbin <jinbin.jb@alibaba-inc.com>
2016-07-27 14:03:05 +0800

    first push base project with nothing

---
+++ POScoreView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POScoreView
+//
+//  Created by admin on 16/7/27.
+//  Copyright © 2016年 kimbin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
